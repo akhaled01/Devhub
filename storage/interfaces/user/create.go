63cf7d0cde5d7d08fa0c8d58e15a8d42baa5d2ae
akhaled <akhaledlarus@gmail.com>
2024-04-08 11:38:40 +0300

    initialized database interfaces

---
+++ storage/interfaces/user/create.go
@@ -0,0 +1,14 @@
+package user
+
+import "RTF/types"
+
+/*
+	This function takes in a user struct and saves
+	the instance in the Database.
+	
+	returns nil if no errors
+*/
+func SaveUserInDB(u types.User) error {
+	
+	return nil
+}

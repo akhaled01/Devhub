ba53005506c69965445bf01b8f6255d296ae4773
akhaled <akhaledlarus@gmail.com>
2024-04-14 00:12:51 +0300

    fmt

--- api/posts/all.go
+++ api/posts/all.go
@@ -9,10 +9,9 @@ import (
 
 /*
 	This is where we get All Posts with No Order
-	GET /posts/all
+		GET /posts/all
 	Refer to the types dir for more info on what a post is
 */
-
 func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&types.Post{})
 }

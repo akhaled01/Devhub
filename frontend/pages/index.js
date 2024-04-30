389a4c70394a86b0a0b18ff46efbeb56eaa41ea1
fatabbas <fatima77614@gmail.com>
2024-04-30 21:07:06 +0300

    postes fetch added.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,144 +8,39 @@ import hashtag from "../images/hashtag.svg"
 export const Home = () => {
   document.getElementById("app").innerHTML = /*html*/ `
     ${LoadNav()}
-<main>
-  <div id="c-post-modal" class="modal">
-  <div class="modal-content">
-    <div id="c-post-userinfo">
-      <div id="c-post-pfp"></div>
-      <p id="c-post-nickname">_.ak79</p>
-    </div>
-    <textarea id="c-post-textArea" placeholder="What's on your mind?"></textarea>
-    <div id="c-post-options">
-      <div class="c-post-option">
-        <img src="${imgupload}" alt="upload Image" title="upload Image" id="c-img-upload">
-        <input type="file" id="img-upload">
-      </div>
-      <div class="c-post-option">
-        <img src="${hashtag}" alt="Choose Category" title="Choose Category" id="cat-choose-Btn">
-      </div>
-    </div>
-    <div id="c-post-cats">
-      <select id="c-post-cat-select">
-        <option class="c-option" value="general">General</option>
-        <option class="c-option" value="general">Dev</option>
-        <option class="c-option" value="general">News</option>
-        <option class="c-option" value="general">Non Fiction</option>
-      </select>
-    </div>
-    <div id="c-post-Btn">Create Post</div> 
-  </div>
-</div>
-  <div id="posts">
-    <div class="f-post">
-      <div class="p-header">
-        <div class="p-profileInfo">
-          <div class="p-profile-pic"></div>
-          <div class="p-nickname">Ralph</div>
-        </div>
-        <div class="p-creationDate">2 Hours Ago</div>
-      </div>
-      <div class="p-main">
-        <div class="p-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas id
-          unde quisquam enim ullam ex quaerat velit numquam autem temporibus.
-          Aut ex vel  necessitatibus, optio maxime debitis! Quo, inventore
-          ducimus!
-          </p>
-          <div class="p-image">
-            <img
-              src="https://images.pexels.com/photos/8434281/pexels-photo-8434281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt="stuff">
-          </div>
-        </div>
-        <div class="p-stats">
-          <div class="p-likeCount">
-            <div class="p-likeBtn">
-              <img src="${noheart}" alt="like" />
+    <main>
+    <div id="c-post-modal" class="modal">
+        <div class="modal-content">
+            <div id="c-post-userinfo">
+                <div id="c-post-pfp"></div>
+                <p id="c-post-nickname">_.ak79</p>
             </div>
-            <div class="p-likeStat">9</div>
-          </div>
-          <div class="p-commentCount">
-            <img src="${comment}" alt="comment" />
-            <div class="p-comment-Stat">10</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="f-post">
-      <div class="p-header">
-        <div class="p-profileInfo">
-          <div class="p-profile-pic"></div>
-          <div class="p-nickname">Ralph</div>
-        </div>
-        <div class="p-creationDate">2 Hours Ago</div>
-      </div>
-      <div class="p-main">
-        <div class="p-content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas id
-          unde quisquam enim ullam ex quaerat velit numquam autem temporibus.
-          Aut ex vel <br> necessitatibus, optio maxime debitis! Quo, inventore
-          ducimus!
-          <div class="p-image">
-            <img
-              src="https://images.pexels.com/photos/8434281/pexels-photo-8434281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt="stuff">
-          </div>
-        </div>
-        <div class="p-stats">
-          <div class="p-likeCount">
-            <div class="p-likeBtn">
-              <img src="${noheart}" alt="like" />
+            <textarea id="c-post-textArea"
+                placeholder="What's on your mind?"></textarea>
+            <div id="c-post-options">
+                <div class="c-post-option">
+                    <img src="${imgupload}" alt="upload Image"
+                        title="upload Image" id="c-img-upload">
+                    <input type="file" id="img-upload">
+                </div>
+                <div class="c-post-option">
+                    <img src="${hashtag}" alt="Choose Category"
+                        title="Choose Category" id="cat-choose-Btn">
+                </div>
             </div>
-            <div class="p-likeStat">9</div>
-          </div>
-          <div class="p-commentCount">
-            <img src="${comment}" alt="comment" />
-            <div class="p-comment-Stat">10</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="f-post noimage">
-      <div class="p-header">
-        <div class="p-profileInfo">
-          <div class="p-profile-pic"></div>
-          <div class="p-nickname">Ralph</div>
-        </div>
-        <div class="p-creationDate">2 Hours Ago</div>
-      </div>
-      <div class="p-main">
-        <div class="p-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas id
-          unde quisquam enim ullam ex quaerat velit numquam autem temporibus.
-          Aut ex vel necessitatibus, optio maxime debitis! Quo, inventore
-          ducimus!
-          </p>
-          <!-- <div class="p-image">
-            <img
-              src="https://images.pexels.com/photos/8434281/pexels-photo-8434281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt="stuff">
-          </div> -->
-        </div>
-        <div class="p-stats">
-          <div class="p-likeCount">
-            <div class="p-likeBtn">
-              <img src="${noheart}" alt="like" />
+            <div id="c-post-cats">
+                <select id="c-post-cat-select">
+                    <option class="c-option" value="general">General</option>
+                    <option class="c-option" value="general">Dev</option>
+                    <option class="c-option" value="general">News</option>
+                    <option class="c-option" value="general">Non
+                        Fiction</option>
+                </select>
             </div>
-            <div class="p-likeStat">9</div>
-          </div>
-          <div class="p-commentCount">
-            <img src="${comment}" alt="comment" />
-            <div class="p-comment-Stat">10</div>
-          </div>
+            <div id="c-post-Btn">Create Post</div>
         </div>
-      </div>
     </div>
-  </div>
+    <div id="posts"></div>
 </main>
   `
 
@@ -205,3 +100,80 @@ export const Home = () => {
   });
 
 }
+
+export async function fetchPost() {
+  const response = await fetch(`/post/all`);
+  const data = await response.json();
+
+  const postDiv = document.getElementById("post");
+  if (data.image) {
+    postDiv.innerHTML = `
+          <div class="f-post">
+              <div class="p-header">
+                  <div class="p-profileInfo">
+                      <div class="p-profile-pic"></div>
+                      <div class="p-nickname">${data.author}</div>
+                  </div>
+                  <div class="p-creationDate">${data.creationDate}</div>
+              </div>
+              <div class="p-main">
+                  <div class="p-content">
+                      ${data.content}
+                      <div class="p-image">
+                          <img src="${data.image}" alt="post image">
+                      </div>
+                  </div>
+                  <div class="p-stats">
+                      <div class="p-likeCount">
+                          <div class="p-likeBtn">
+                              <img src="${noheart}" alt="like" />
+                          </div>
+                          <div class="p-likeStat">${data.likes}</div>
+                      </div>
+                      <div class="p-commentCount">
+                          <img src="${comment}" alt="comment" />
+                          <div class="p-comment-Stat">${data.comments}</div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      `;
+  } else {
+    postDiv = document.getElementById("post");
+    postDiv.innerHTML = `
+        < -- categories should be connected to the backend when it's done.  -- >
+        <div class="f-post noimage">
+            <div class="p-header">
+                <div class="p-profileInfo">
+                    <div class="p-profile-pic"></div>
+                    <div class="p-nickname">${data.author}</div>
+                </div>
+                <div class="p-creationDate">${data.creationDate}</div>
+            </div>
+            <div class="p-main">
+                <div class="p-content">
+                    ${data.content}
+                </div>
+                <div class="p-stats">
+                    <div class="p-likeCount">
+                        <div class="p-likeBtn">
+                            <img src="${noheart}" alt="like" />
+                        </div>
+                        <div class="p-likeStat">${data.likes}</div>
+                    </div>
+                    <div class="p-commentCount">
+                        <img src="${comment}" alt="comment" />
+                        <div class="p-comment-Stat">${data.comments}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+  }
+}
+// categories for later.
+/* export async function fetchPost() {
+  const response = await fetch(``);
+  const data = await response.json();
+  const postDiv = document.getElementById("post");
+} */

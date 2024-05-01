200ea3ef697fdaf3522c25ad860280c9428b75d8
Mohammed AlSammak <msk170a@gmail.com>
2024-05-02 00:01:36 +0300

    meaningful info for terminal

--- storage/interfaces/chat/create.go
+++ storage/interfaces/chat/create.go
@@ -22,10 +22,10 @@ func SaveChatInDB(chat serializers.Message) error {
 	}
 	defer stmt.Close()
 
-	if result, err := stmt.Exec(chat.Msg_Content, chat.Timestamp.String(), chat.Sender, chat.Recipient); err != nil {
+	if _, err := stmt.Exec(chat.Msg_Content, chat.Timestamp, chat.Sender, chat.Recipient); err != nil {
 		return errors.Join(errors.New("error executing SaveChatMessageInDB query"), err)
 	} else {
-		utils.InfoConsoleLog(fmt.Sprintf("New chat/message created: %s", result))
+		utils.InfoConsoleLog(fmt.Sprintf("New chat/message created: \n{\n%s\n}\n", chat.To_String()))
 	}
 
 	return nil
